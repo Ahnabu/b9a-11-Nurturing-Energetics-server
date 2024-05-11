e01057eb5c22252f0d755dfc89e2f6ecf1cb7501
Ahnabu <horairaabu013025@gmail.com>
2024-05-11 20:22:40 +0600

    single page data loaded

--- index.js
+++ index.js
@@ -60,17 +60,17 @@ async function run() {
         app.get('/', (req, res) => {
             res.send('Hello World!')
         })
-        app.get('/top', async (req, res) => {
+        app.get('/top', async (req, res) => { 
             const sort = req.query.sort
             const result = await restaurantDB.find().sort({ purchase_amount: -1 }).limit(6).toArray();
             res.send(result)
         })
         app.get('/details/:id', async (req, res) => {
-            const id = req.params.id
-            const query = {_id : new ObjectId(id)}
-            const result = await restaurantDB.findOne(query).toArray();
-            res.send(result)
-        })
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await restaurantDB.findOne(query);
+            res.send(result);
+        }) 
         app.get('/all', async (req, res) => {
             
             const filter = req.query.filter
